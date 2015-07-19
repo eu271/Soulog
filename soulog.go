824f2b48830d353dd669ab570e0ba94c0007478b
Eugenio Ochoa Lopez <eugenio_oso@hotmail.com>
2015-07-19 14:29:22 +0200

    Added first template for Posts.

--- soulog.go
+++ soulog.go
@@ -33,19 +33,22 @@ func main() {
 
 	http.HandleFunc("/", ServirIndex)
 
-	log.Println("Registrando las direcciones del blog.")
+	log.Println("Adding BlogResources to the URLs available to send.")
 	registrarDireccionFichero("/blog.css", "./cliente/blog.css", "text/css")
 	registrarDireccionFichero("/blog.js", "./cliente/blog.js", "application/javascript")
 	registrarDireccionFichero("/javascript_global/sha256.js", "./cliente/javascript_global/sha256.js", "application/javascript")
 	registrarDireccionFichero("/javascript_global/variables.js", "./cliente/javascript_global/variables.js", "application/javascript")
 	registrarDireccionFichero("/javascript_global/mmd.js", "./cliente/javascript_global/mmd.js", "application/javascript")
 
-	log.Println("Registrando direcciones de administracion")
+	log.Println("Adding AdminResourses to the URLs available to send.")
 	registrarDireccionFichero("/admin", "./cliente/soul/soul.html", "text/html")
 	registrarDireccionFichero("/soul.css", "./cliente/soul/soul.css", "text/css")
 	registrarDireccionFichero("/soul.js", "./cliente/soul/soul.js", "application/javascript")
+	
+	log.Println("Adding Templates to the URLs available to send.")
+	registrarDireccionFichero("/post.html.mustache", "./cliente/templates/post.html.mustache", "text/html")
 
-	log.Println("Iniciando el servidor.")
+	log.Println("Starting server...")
 	/*
 			go func(){
 				http.ListenAndServeTLS(":8088", )
